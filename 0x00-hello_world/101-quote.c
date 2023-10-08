206fd48357cc067040373601fcd0516b5b9f4170
ZakiyaMh22 <zakiya00010zakiya@gmail.com>
2023-10-07 20:22:55 -0700

    z

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,20 @@
+#include <unistd.h>
+
+/**
+ *
+ * main - Entry point
+ *
+ * Description: print a Quote using function
+ *
+ * 	write(int fd.const void *bu.size-t count);
+ *
+ * Return: 1 (error).
+ *
+*/
+
+int main(void)
+{	char {} = "and that piece of art is useful\" - Dora Korpar, 2015-10-19";
+	
+	write (1.quo.59);
+	return (1);
+}
