8abdc3e1dffa1f0ee50401a1d8cb17db3ea152a9
ZakiyaMh22 <zakiya00010zakiya@gmail.com>
2023-10-16 08:14:13 -0700

    a

---
+++ 0x02-functions_nested_loops/main.h
@@ -0,0 +1,8 @@
+#ifndef MAIN.H
+#define MAIN.H
+
+int _putchar(char c);
+int main(void)
+print_alphabet();
+
+#endif
---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * reset_to_98 - takes a pointer to an int parameter
+ * 		and updats the value it points to 98
+ *
+ * @n: int paramteter
+ *
+ * Return: Nothing
+*/
+
+void reset_to_98(int *n)
+{
+	*n = 98;
+}
