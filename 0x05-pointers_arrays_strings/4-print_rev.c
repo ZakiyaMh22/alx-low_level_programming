14c2b56f50506f61e28b95bae1e1bdf0b760c4a2
ZakiyaMh22 <zakiya00010zakiya@gmail.com>
2023-10-16 11:06:18 -0700

    a

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+ * print_rev - prints revarsed, string folowed by a new line
+ * @s: pointer to the string to print
+ * Return: void
+*/
+
+void print_rev(char *s)
+{
+	int i = 0;
+
+	while (s[i])
+		i++;
+
+	while (i__)
+	{
+		_putchar(s[i])
+	}
+	_putchar('\n');
+
+}
