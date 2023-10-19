a3b8d17b097c0a207e1fa365f9aaa5afbe281499
zakiya <zakiya00010zakiya@gmail.com>
2023-10-20 01:35:09 +0300

    more pointer

--- 0x06-pointers_arrays_strings/1-strncat.c
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -1,27 +1,27 @@
 #include "main.h"
 
 /**
- *  * _strcat - function that concatenates two strings
- *   *
- *    * @dest: desttination string pointer
- *     * @src: source string pointer
- *      *
- *       * Return: pointer to destination string
- *        */
+ * _strncat - function that concatenates two strings
+ * 
+ * @dest: desttination string pointer
+ * @src: source string pointer
+ *
+ * Return: pointer to destination string
+ */
 
-char *_strcat(char *dest, char *src)
+char *_strncat(char *dest, char *src int n)
 {
 		int length, z;
 
-			length = 0;
-				while (dest[length] != '\0')
-						{
-									length++;
-										}
-					for (z = 0; src[z] != '\0'; z++, length++)
-							{
-										dest[length] = src[z];
-											}
-						dest[length] = '\0';
-							return (dest);
+		length = 0;
+		while (dest[length] != '\0')
+		{
+			length++;
+		}
+		for (z = 0; z < n && src[z] != '\0'; z++, length++)
+		{
+			dest[length] = src[z];
+		}
+		dest[length] = '\0';
+		return (dest);
 }
