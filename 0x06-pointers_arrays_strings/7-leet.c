da0eecd23e8d329ac401d499f6544a144b09fded
zakiya <zakiya00010zakiya@gmail.com>
2023-10-20 13:13:23 +0300

    motr pointer

--- 0x06-pointers_arrays_strings/7-leet.c
+++ 0x06-pointers_arrays_strings/7-leet.c
@@ -1,45 +1,36 @@
-#include "main.h"
+nclude "main.h"
 
 /**
  * leet - function that encodes a string into 1337
- * 
  * Letters a and A should be replaced by 4
- *
  * Letters e and E should be replaced by 3
- *
  * Letters o and O should be replaced by 0
- *
  * Letters t and T should be replaced by 7
- *
  * Letters l and L should be replaced by 1
+ * @s: pointer to string
  *
- * @a: pointer to string
- *
- * Return: pointer to a
+ * Return: pointer to s
  */
 
-char *leet(char *a)
+char *leet(char *s)
 {
-	int i = 0;
-       	while (a[i] != '\0')
-	{
-		if (a[i] == 'a' || a[i] == 'A'){
-				a[i] = '4';
-		}
-		else if (a[i] == 'e' || a[i] == 'E'){
-				a[i] = '3';
-		}
-		else if (a[i] == 'o' || a[i] == 'O'){
-				a[i] = '0';
-		}
-		else if (a[i] == 't' || a[i] == 'T'){
-			a[i] = '7';
-		}
-		else if (a[i] == 'l' || a[i] == 'L'){
-			a[i] = '1';
-		}
-		i++;
-	}
+	int string_length, leetCount;
+	char leetLetters[] = "aAeEoOtTlL";
+	char leetNums[] = "4433007711";
+	string_length = 0;
 
-	return a;
+	while (s[string_length] != '\0')
+	{
+		leetCount = 0;
+		while (leetCount < 10)
+		{
+			if (leetLetters[leetCount] == s[string_length])	
+			{
+				s[string_length] = leetNums[leetCount];
+			}
+		leetCount++;
+		}
+	string_length++;
+	}
+	return (s);
 }
