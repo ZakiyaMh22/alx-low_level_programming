0299523aeb09abeee5dc41e3ddee51e2798601c3
zakiya <zakiya00010zakiya@gmail.com>
2023-10-20 12:18:52 +0300

    motr pointer

--- 0x06-pointers_arrays_strings/4-rev_array.c
+++ 0x06-pointers_arrays_strings/4-rev_array.c
@@ -1,24 +1,24 @@
 #include "main.h"
 
 /**
- *  * reverse_array -  function that reverses the content of an array of integers
- *   *
- *    * @a: pointer to array
- *     * @n: number of elements of an array
- *      *
- *       * Return: Nothing
- *        */
+ * reverse_array -  function that reverses the content of an array of integers
+ *
+ * @a: pointer to array
+ * @n: number of elements of an array
+ *
+ * Return: Nothing
+ */
 
 void reverse_array(int *a, int n)
 {
 		int temp, i;
 
-			n = n - 1;
-				i = 0;
-					while (i <= n)
-							{
-										temp = a[i];
-												a[i++] = a[n];
-														a[n--] = temp;
-															}
+		n = n - 1;
+		i = 0;
+		while (i <= n)
+		{
+			temp = a[i];
+			a[i++] = a[n];
+			a[n--] = temp;
+		}
 }
