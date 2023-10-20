03ac909dcdb676f862434dea8c2e0ca1e4c49491
zakiya <zakiya00010zakiya@gmail.com>
2023-10-20 12:24:42 +0300

    motr pointer

--- 0x06-pointers_arrays_strings/5-string_toupper.c
+++ 0x06-pointers_arrays_strings/5-string_toupper.c
@@ -1,27 +1,28 @@
 #include "main.h"
 
 /**
- *  * string_toupper -  function that changes all lowercase letters
- *   *		of a string to uppercase
- *    *
- *     * @s: pointer to string
- *      *
- *       * Return: pointer to uppercase string
- *        */
+ * string_toupper -  function that changes all lowercase letters
+ *		of a string to uppercase
+ *
+ * @s: pointer to string
+ *
+ * Return: pointer to uppercase string
+ */
 
 char *string_toupper(char *s)
 {
 		int length_of_string;
 
-			length_of_string = 0;
+		length_of_string = 0;
 
-				while (s[length_of_string] != '\0')
-						{
-									if (s[length_of_string] >= 'a' && s[length_of_string] <= 'z')
-												{
-																s[length_of_string] = s[length_of_string] - 32;
-																		}
-											length_of_string++;
-												}
-					return (s);
+		while (s[length_of_string] != '\0')
+		{
+			if (s[length_of_string] >= 'a' && s[length_of_string] <= 'z')
+			{
+				s[length_of_string] = s[length_of_string] - 32;
+																
+			}
+		length_of_string++;
+		}
+		return (s);
 }
