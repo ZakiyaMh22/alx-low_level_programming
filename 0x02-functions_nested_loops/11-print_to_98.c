530227d19cb788063aeae1888500e164105dd8d5
zakiya <zakiya00010zakiya@gmail.com>
2023-10-20 15:52:12 +0300

    function

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -20,12 +20,11 @@ void print_to_98(int n)
 			printf("%d, ", count);
 		}
 	}
-	else
+	else{
+	for (count = n; count < 98; count++)
 	{
-		for (count = n; count < 98; count++)
-		{
-				printf("%d, ", count);
-		}
+		printf("%d, ", count);
 	}
+}
 	printf("98\n");
 }
