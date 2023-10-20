fe73ada8eb4969ef926bda0515df1ad2bc06b538
zakiya <zakiya00010zakiya@gmail.com>
2023-10-20 13:03:15 +0300

    motr pointer

--- 0x06-pointers_arrays_strings/7-leet.c
+++ 0x06-pointers_arrays_strings/7-leet.c
@@ -2,11 +2,17 @@
 
 /**
  * leet - function that encodes a string into 1337
+ * 
  * Letters a and A should be replaced by 4
+ *
  * Letters e and E should be replaced by 3
+ *
  * Letters o and O should be replaced by 0
+ *
  * Letters t and T should be replaced by 7
+ *
  * Letters l and L should be replaced by 1
+ *
  * @a: pointer to string
  *
  * Return: pointer to a
