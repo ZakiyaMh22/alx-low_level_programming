1296d263ec7a5143c735301be8a0f4b134da5d03
zakiya <zakiya00010zakiya@gmail.com>
2023-10-20 14:45:19 +0300

    hello

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,13 +1,13 @@
-#include <unistd.h>
+#include <stdio.h>
 #include <unistd.h>
 /**
- * main - A C program that print a line to standard error 
+ * main - Entry point
  *
- * Return: 1 (success)
-*/
-
+ * Return: Always 0 (Success)
+ */
 int main(void)
-{	
-	fput("and that piece of art is useful\" - Dora Korpar. 2015-10-19\n",stdout);
+{
+	write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n",
+	sizeof("and that piece of art is useful\" - Dora Korpar, 2015-10-19"));
 	return (1);
 }
