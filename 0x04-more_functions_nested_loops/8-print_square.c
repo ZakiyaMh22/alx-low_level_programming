fb3899c1947d5bb3588b67134f0670e688f6eb1c
zakiya <zakiya00010zakiya@gmail.com>
2023-10-21 21:52:23 +0300

    more function

---
+++ 0x04-more_functions_nested_loops/8-print_square.c
@@ -0,0 +1,30 @@
+#include "main.h"
+
+/**
+ * print_squar - prints a squar of specified size
+ *
+ * @size: size of the square
+ *
+ * Return: void
+ */
+
+void print_square(int size)
+{
+	int row, coulmn;
+
+	if (size <= 0)
+	{
+	_putchar('\n');
+	}
+	else
+	{
+	for (row = 0; row < size; row++)
+	{
+		for (coulmn = 0; coulmn < size; coulmn++)
+
+			_putchar('#');
+
+		_putchar('\n');
+	}
+	}
+}
