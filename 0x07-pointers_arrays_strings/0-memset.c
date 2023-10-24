9f41c8fe9a1a0c00bba68e284caa98012361e022
ZakiyaMh22 <zakiya00010zakiya@gmail.com>
2023-10-24 04:01:39 -0700

    task 0

---
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * _memset - library function with conest byet
+ * @s : is pointer to put te constant
+ * @b: conestant
+ * @n: the max byet
+ * Return: pointer s
+*/
+char *_memset(char *s, char b, unsigned int n);
+{
+	unsigned int root;
+
+	for (root = 0; n > 0; root++, n--)
+	{
+		s[root] = b;
+	}
+	return (s);
+}
