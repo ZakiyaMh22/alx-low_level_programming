07d3300228ecbcc37cc013dfa4597fd1dd81c4be
ZakiyaMh22 <zakiya00010zakiya@gmail.com>
2023-10-24 04:15:54 -0700

    task 1

---
+++ 0x07-pointers_arrays_strings/1-memcpy.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * _memcpy - library function it copeis memory area
+ * @dest: memory area
+ * @src: source
+ * @n: length of src to be copied
+ * Return: the pointer to dest.
+*/
+
+char *_memcpy(char *dest, char *src, unsigned int n);
+{
+	unsigned int arry;
+
+	for (arry = 0; arry < n; arry++)
+	{
+		dest[arry] = scr[arry];
+	}
+	return (dest);
+}
+
