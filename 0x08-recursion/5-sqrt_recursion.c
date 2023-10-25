164dab1551c55d8ef1e9875f9da82a54755d0545
ZakiyaMh22 <zakiya00010zakiya@gmail.com>
2023-10-25 12:43:05 -0700

    advance2

--- 0x08-recursion/5-sqrt_recursion.c
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -12,6 +12,12 @@ int _sqrt_recursion(int n)
 {
 	return (square(n, 1));
 }
+/**
+ * square - find square root
+ * @n: int to find square root
+ * @val: square root
+ * Return: int value
+*/
 int square(int n, int val)
 {
 	if (val * val == n)
