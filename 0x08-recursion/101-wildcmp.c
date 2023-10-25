497855b9db5223ac4d97dcfa546133195768368f
ZakiyaMh22 <zakiya00010zakiya@gmail.com>
2023-10-25 13:16:36 -0700

    101-wildcmp.c

--- 0x08-recursion/101-wildcmp.c
+++ 0x08-recursion/101-wildcmp.c
@@ -1,68 +1,29 @@
 #include "main.h"
 
 /**
- * move_past_star - iterates past asterisk
- * @s2: second string, contain wildcard
- * Return: the pointer move past star
-*/
-
-char *move_past_star(char *s2)
-{
-	if (*s2 == '*')
-		return (move_past_star(s2 + 1));
-	else
-		return (s2);
-}
-/**
- * inception - makes magic a reality
- * @s1: the first string
- * @s2: second string, contain wildcare
- * Return: 1 if identical, 0 if false
-*/
-int inception(char *s1, char *s2)
-{
-	int ret = 0;
-
-	if (*s1 == 0)
-		return (0);
-	if (*s1 == *s2)
-		ret += wildcmp(s1 + 1, s2 + 1);
-	ret += inception(s1 + 1, s2);
-	return (ret);
-}
-
-/**
  * wildcmp - comper two strigs lexicographical
  * @s1: first string
  * @s2: second string,cpntain wildcard
- * Return: 1 if identical, 0 if false
+ * Return: 0
 */
 
 int wildcmp(char *s1, char *s2)
 {
-	int ret = 0;
-
-	if (!*s1 && *s2 == '*' || !*move_past_star(s2))
-	return (1);
-	if (*s1 == *s2)
-{
-	if (!*s1)
-		return (1);
-	return (wildcmp(s1 + 1, *s2 != '*' ? s2 : s1 + 1));
-}
-if (!*s1 || !s2)
-	return (0);
+	if (*s1 == '\0')
+	{
+		if (*s2 != '\0' && *s2 == '*')
+		{
+			return (wildcmp(s1, s2 + 1));
+		}
+		return (*s2 == '\0');
+	}
 	if (*s2 == '*')
-{
-	s2 = move_past_star(s2);
-	if (!*s2)
-		return (1);
-	if (*s1 == *s2)
-		ret += wildcmp(s1 + 1, s2 + 1);
-	ret += inception(s1, s2);
-	return (!!ret);
+	{
+		return (wildcmp(s1 + 1, s2) || wildcmp(s1, s2 + 1));
+	}
+	else if (*s2 == *s2)
+	{
+		return (wildcmp(s1 + 1, s2 + 1));
+	}
+	return (0);
 }
-return (0);
-}
-
-
