075a5f64703a0adbf1d98093584bf51c0e329f6f
ZakiyaMh22 <zakiya00010zakiya@gmail.com>
2023-10-30 03:31:12 -0700

    3-mul.c

--- 0x0A-argc_argv/3-mul.c
+++ 0x0A-argc_argv/3-mul.c
@@ -1,17 +1,18 @@
 #include <stdio.h>
 #include <stdlib.h>
+
 /**
  * main - function that multiplcation of two number
  * @argc: int
  * @argv: char vector
- * @sum: multiplcation
  * Return: 1 (success), 0 if not
 */
-int main(int argc,char *argv[])
+
+int main(int argc, char *argv[])
 {
 	int sum;
 
-	if(argc == 3)
+	if (argc == 3)
 	{
 		sum = atoi(argv[1]) * atoi(argv[2]);
 		printf("%d\n", sum);
