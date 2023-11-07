ba308000c9f0f20217b75200451ae176a15964f1
ZakiyaMh22 <zakiya00010zakiya@gmail.com>
2023-11-07 03:55:51 -0800

    1-init_dog.c

--- 0x0E-structures_typedef/1-init_dog.c
+++ 0x0E-structures_typedef/1-init_dog.c
@@ -1,4 +1,4 @@
-#include <stdio.h>
+#include "dog.h"
 
 /**
  * init_dog - nitialize a variable of type struct dog
@@ -10,7 +10,10 @@
 */
 void init_dog(struct dog *d, char *name, float age, char *owner)
 {
-	d->name = name;
-	d->age = age;
-	d->owner = owner;
+	if (d)
+	{
+		d->name = name;
+		d->age = age;
+		d->owner = owner;
+	}
 }
