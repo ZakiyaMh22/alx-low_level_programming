3f0f4073c9e102c7d755003fb3b3ed90f6401ed6
ZakiyaMh22 <zakiya00010zakiya@gmail.com>
2023-11-07 03:49:37 -0800

    This my first commit

---
+++ 0x0E-structures_typedef/1-init_dog.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+/**
+ * init_dog - nitialize a variable of type struct dog
+ * @name: Dog's name
+ * @age: Dog's age
+ * @owner: Dog's owner
+ * @d: its pointer to point of struct element
+ * Return: void
+*/
+void init_dog(struct dog *d, char *name, float age, char *owner)
+{
+	d->name = name;
+	d->age = age;
+	d->owner = owner;
+}
--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -11,9 +11,13 @@
 
 struct dog
 {
-	char* name;
+	char *name;
 	float age;
-	char* owner;
+	char *owner;
 };
+
+void init_dog(struct dog *d, char *name, float age, char *owner);
+void print_dog(struct dog *d);
+dog_t *new_dog(char *name, float age, char *owner);
 
 #endif
