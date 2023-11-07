27618124f7005e370c41c0bedbd40d8b4cbccf1e
ZakiyaMh22 <zakiya00010zakiya@gmail.com>
2023-11-07 09:12:02 -0800

    my first commit

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,17 @@
+#ifndef "DOG_H"
+#define "DOG_H"
+/**
+ * struct dog - a dog struct
+ * @name: dog's name
+ * @age: float dog's age
+ * @owner: char dog's owner
+ * Description: is the first struct
+*/
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+};
+
+#endif
