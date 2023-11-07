bb16cba6a102d85395e529e7e04f9f4f435a20ea
ZakiyaMh22 <zakiya00010zakiya@gmail.com>
2023-11-07 05:23:14 -0800

    This is my first commit

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -3,21 +3,22 @@
 
 /**
  * struct dog - a dog struct
- *@name: string of name of dog
- * @age: is the age of dog
- * @owner: is the parson who own the dog
- * Descripition: just a lone dog struct
+ * @name: name of dog
+ * @age: age of dog
+ * @owner: owner's dog
 */
-
 struct dog
 {
 	char *name;
 	float age;
 	char *owner;
 };
-
 void init_dog(struct dog *d, char *name, float age, char *owner);
-void print_dog(struct dog *d);
+struct dog
+
+Prototype: void print_dog(struct dog *d);
 dog_t *new_dog(char *name, float age, char *owner);
 
-#endif
+#endif /* DOG_H */
+
+
