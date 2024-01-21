c047976bc8723d828bcf473eaa74974da9139bb7
ZakiyaMh22 <zaliya00010zakiya@gmail.com>
2024-01-21 12:03:08 +0200

    1-strdup.c

---
+++ 0x0B-malloc_free/1-strdup.c
@@ -0,0 +1,32 @@
+#include "main.h"
+#include <stdlib.h>
+#include <string.h>
+
+/**
+ * _strdup - unction returns a pointer to the duplicated string
+ *
+ * @str: string which is a duplicate of the string
+ *
+ * Return: returns a pointer
+*/
+char *_strdup(char *str)
+{
+	char *duplicate;
+	size_t len = strlen(str);
+
+	if (str == NULL)
+	{
+		return (NULL);
+	}
+
+	duplicate = (char *)malloc((len + 1) * sizeof(char));
+
+		if (duplicate == NULL)
+		{
+			return (NULL);
+		}
+
+	strcpy(duplicate, str);
+
+	return (duplicate);
+}
