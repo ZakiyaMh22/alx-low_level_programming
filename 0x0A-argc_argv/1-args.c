318379d3355a96d5b85b9b24c1cb34c29ccf169a
ZakiyaMh22 <zaliya00010zakiya@gmail.com>
2024-02-22 14:45:35 +0200

    hello

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+/**
+ * main - entry point
+ * @argc: int
+ * @argv: list
+ * Return: 0
+*/
+int main(int argc, char const *argv[])
+{
+	(void)argv;
+	printf("%d\n", argc - 1);
+	return (0);
+}
+
